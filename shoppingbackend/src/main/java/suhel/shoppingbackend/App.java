28e562be2e99400c7dbdcca122e5ec6c34631e87
suhel sande <suhelsande2015@gmail.com>
2018-01-25 12:48:17 -0800

    My project set up successfully

---
+++ shoppingbackend/src/main/java/suhel/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package suhel.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
